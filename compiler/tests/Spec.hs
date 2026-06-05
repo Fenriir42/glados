319ca3e822b85a425711c27d452bd4d23ff737c9
jbregent <jbregent@tranquil.it>
2026-06-05 14:08:36 +0200

    test(compiler): add nested-elif and file compilation specs

--- compiler/tests/Spec.hs
+++ compiler/tests/Spec.hs
@@ -1,13 +1,60 @@
 module Main (main) where
 
+import AST.Types.AST (Program (..))
+import Compiler.Bytecode (Bytecode (..), Instruction (..))
+import Compiler.Codegen (compileProgram)
+import Control.Monad (forM_)
+import Lib (lexFile)
+import Parser.Decl (parseDecl)
 import Test.Hspec
+import Text.Megaparsec (errorBundlePretty, many, runParser)
 
-dummySpec :: Spec
-dummySpec = do
-  describe "dummy test" $ do
-    it "Should always pass" $ do
-      True `shouldBe` True
+-- | Attempt to parse and compile a file, returning an error string or the
+-- compiled bytecodes.
+compileFile :: FilePath -> IO (Either String [Bytecode])
+compileFile filePath = do
+  lexResult <- lexFile filePath
+  case lexResult of
+    Left lexErr -> return $ Left $ "Lexing error: " ++ lexErr
+    Right tokens ->
+      case runParser (many parseDecl) filePath tokens of
+        Left parseErr ->
+          return $ Left $ "Parse error: " ++ errorBundlePretty parseErr
+        Right decls ->
+          case compileProgram (Program decls) of
+            Left compileErr -> return $ Left $ "Compile error: " ++ show compileErr
+            Right bytecodes -> return $ Right bytecodes
+
+displayBytecode :: Bytecode -> String
+displayBytecode bc =
+  unlines $
+    [ "Function: " ++ show (bytecodeFunction bc),
+      "Instructions (" ++ show (length (bytecodeInstructions bc)) ++ "):"
+    ]
+      ++ zipWith (\i instr -> "  " ++ pad i ++ ": " ++ show instr) [0 ..] (bytecodeInstructions bc)
+      ++ ( if null (bytecodeStrings bc)
+             then []
+             else
+               "String pool:"
+                 : zipWith (\i s -> "  [" ++ show i ++ "] " ++ show s) [0 ..] (bytecodeStrings bc)
+         )
+  where
+    pad i = let s = show (i :: Int) in replicate (4 - length s) ' ' ++ s
+
+fileSpec :: FilePath -> Int -> Spec
+fileSpec path expectedFunctions = do
+  it ("Should compile " ++ path) $ do
+    result <- compileFile path
+    case result of
+      Left err -> expectationFailure err
+      Right bytecodes -> do
+        length bytecodes `shouldBe` expectedFunctions
+        forM_ bytecodes $ \bc ->
+          putStrLn $ "\n" ++ displayBytecode bc
 
 main :: IO ()
 main = hspec $ do
-  dummySpec
+  describe "File compilation" $ do
+    fileSpec "/root/glados/tests/nested_elif.qa" 1
+    fileSpec "/root/glados/tests/nth_prime.qa" 2
+    fileSpec "/root/glados/tests/array.qa" 1
